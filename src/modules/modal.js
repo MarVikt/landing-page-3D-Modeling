1dd88b21a946f0e9c5e912afd58b84999a3c6eea
MarVikt <v.m.podporogie@gmail.com>
2022-04-23 18:17:00 +0300

    completed task#20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,16 +1,29 @@
 const modal = () => {
-  const buttons = document.querySelectorAll('.popup-btn');
-  const popup = document.querySelector('.popup');
-  const popupClose = popup.querySelector('.popup-close');
+  const buttons = document.querySelectorAll(".popup-btn");
+  const popup = document.querySelector(".popup");
+  const popupClose = popup.querySelector(".popup-close");
+  const content = popup.querySelector(".popup-content");
+
+  function showModal() {
+    popup.style.display = "block";
+    if (window.screen.availWidth > 767) {
+      let count = 0;
+      content.style.opacity = "0";
+      let idInterval = setInterval(() => {
+        count ++; 
+        content.style.opacity = String(count / 10);
+        if (count == 10) clearInterval(idInterval);
+      },30);
+    }
+  }
 
   buttons.forEach((btn) => {
-    btn.addEventListener('click', () => {
-      popup.style.display = "block";
-    });
+    btn.addEventListener("click",showModal);
   });
 
-  popupClose.addEventListener('click', () => {
+  popupClose.addEventListener("click", () => {
     popup.style.display = "none";
   });
 };
+
 export default modal;
