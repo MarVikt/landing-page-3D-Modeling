1d5f53c31c43008793708045882aad05c4fa4660
MarVikt <v.m.podporogie@gmail.com>
2022-04-25 14:46:56 +0300

    created tabs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import tabs from "./modules/tabs";
 
 timer('28 april 2022');
 menu();
 modal();
+tabs();
