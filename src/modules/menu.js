0c841ee3cdfbe4fc25fbab80d384ea4984244a77
MarVikt <v.m.podporogie@gmail.com>
2022-04-26 20:03:17 +0300

    fixed task#22 mandatory

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,17 +1,29 @@
 const menu = () => {
   const menu = document.querySelector('menu');
+  const menuBtn = document.querySelector('.menu');
   const body = document.querySelector('body');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
-  body.addEventListener('click', (e) => {
+  menu.addEventListener('click', (e) => {
     if (e.target.classList.contains('close-btn') ||
-        !e.target.closest('.active-menu') ||
-        e.target.matches('.active-menu a')) {
+        e.target.tagName === 'A') {
       handleMenu();
     }
   });
+
+  menuBtn.addEventListener('click',handleMenu);
+
+
+
+  // body.addEventListener('click', (e) => {
+  //   if (e.target.classList.contains('close-btn') ||
+  //       !e.target.closest('.active-menu') ||
+  //       e.target.matches('.active-menu a')) {
+  //     handleMenu();
+  //   }
+  // });
 };
 export default menu;
