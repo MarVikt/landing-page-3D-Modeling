6f871e034f1a6ecf4c0d354f9fe015f040267933
MarVikt <v.m.podporogie@gmail.com>
2022-04-27 11:47:40 +0300

    fixed click to the left point of menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -10,7 +10,7 @@ const menu = () => {
     if (e.target.closest('.menu') || e.target.classList.contains('close-btn') ||
         e.target.matches('.active-menu a')) {
       handleMenu();
-    } else if (!e.target.matches('menu') && menu.classList.contains('active-menu')) {
+    } else if (!e.target.closest('menu') && menu.classList.contains('active-menu')) {
       handleMenu();
     }
   });
