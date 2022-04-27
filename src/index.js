b367f63208ad27575fb66e8f52bfe6a098733bb8
MarVikt <v.m.podporogie@gmail.com>
2022-04-27 12:14:00 +0300

    merge lesson22

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validation from "./modules/validation";
 import calculator from "./modules/calculator";
+import tabs from "./modules/tabs";
 
 timer('28 april 2022');
 menu();
 modal();
 validation();
 calculator();
+tabs();
